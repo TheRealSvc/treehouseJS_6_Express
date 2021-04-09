abce478313671b357af2f009be074e4e319c5d19
SVC <svcoelln@yahoo.de>
2021-04-09 10:03:17 +0200

    just some more unfinished stuff

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const data = require('./data.json');
 
 const app = express();
@@ -13,9 +14,35 @@ app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
+/*
+router.get('/:id', (req, res) => {
+    const { side } = req.query;
+    const { id } = req.params;
 
+    if ( !side ) {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    
+    const templateData = { id, text, name, side };
 
-app.use('/index', (req,res) => {
+    if ( side === 'question' ) {
+      templateData.hint = hint;
+      templateData.sideToShow = 'answer';
+      templateData.sideToShowDisplay = 'Answer';
+    } else if ( side === 'answer' ) {
+      templateData.sideToShow = 'question';
+      templateData.sideToShowDisplay = 'Question';
+    }
+
+    res.render('card', templateData);
+});
+
+*/
+
+app.use('/', (req,res) => {
     res.render('index', {"data": data});
 });
 
